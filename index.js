4b8d39a73da7c96ea31e937de72b07c241488ece
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-09 13:35:53 -0500

    added player cards and some css to style them

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //make state
+//constantly out of scope! -- work with it -- then fix later.
 const state = {
   dogPlayers: [],
   teams: [], //team ruff and team fluff LOL!
@@ -20,26 +21,28 @@ const getPlayersFromAPI = async () => {
   try {
     const response = await fetch(`${baseURL}players`);
     const responseJSON = await response.json();
-    state.dogPlayers = responseJSON.data.players;
+    const roster = responseJSON.data.players;
+    state.dogPlayers = roster;
+    return roster;
     //add error stuff here!!!!
   } catch (error) {
     console.log(`error`);
   }
 };
+
+getPlayersFromAPI();
 
 const getTeamsFromAPI = async () => {
   try {
     const response = await fetch(`${baseURL}teams`);
     const responseJSON = await response.json();
     state.teams = responseJSON.data.teams;
-    console.log(state.teams);
     //add error stuff here!!!!
   } catch (error) {
     console.log(`error`);
   }
 };
 
-//I'm getting the individual player from the API to show I can, although I think I'll mainly be using the players from state
 const getIndividualPlayerFromAPI = async (playerID) => {
   try {
     const response = await fetch(`${baseURL}players/${playerID}`);
@@ -52,4 +55,84 @@ const getIndividualPlayerFromAPI = async (playerID) => {
   }
 };
 
-getIndividualPlayerFromAPI(229);
+
+////////////////////////////////////////////
+/// GET DATA FROM API FOR INITIAL STATE  ///
+////////////////////////////////////////////
+
+//NOTE THIS DOESN'T WORK!!!!
+//can't write to global variable from local scope
+//would work with object oriented programming probably
+
+// getPlayersFromAPI()
+// getTeamsFromAPI()
+// console.log(Object.keys(state))
+// console.log(state.dogPlayers)
+
+
+/////////////////////////
+/// RENDER FUNCTIONS  ///
+/////////////////////////
+const renderNavSection=() => {}
+
+
+///IMPORTANT!!///
+const renderCards = async () => {
+  renderNavSection(); //adds nav TBD
+  ///main cards section
+  state.dogPlayers = await getPlayersFromAPI() //loads up players into state from API
+  
+  const allDogArray = state.dogPlayers.map(dogInfo => {
+    return `
+    <content>
+    <img src="${dogInfo.imageUrl}" alt="dog pic" width=100%>
+    <h2>${dogInfo.name}</h2>
+    </content>`
+  });
+  
+  const cardViewSection = document.createElement(`section`);
+  cardViewSection.id = `cards`;
+  main.appendChild(cardViewSection);
+
+  allDogArray.forEach((dogInfo, index) => {
+    const dogCard = document.createElement(`article`);
+    dogCard.id = (index);
+    cardViewSection.append(dogCard);
+    dogCard.innerHTML=dogInfo
+  });
+}
+
+const renderForm = () => {}
+
+
+// const renderCardMain = () => {
+//   renderNavSection();
+//   renderCards();
+//   renderForm();
+// }
+
+
+
+renderCards();
+
+
+
+
+
+
+
+const renderDetails = () => {}
+const renderTeams = () => {}
+
+///////////////////////////
+/// LISTENER FUNCTIONS  ///
+///////////////////////////
+
+const addCardListeners = () => {}
+const addNavListeners = () => {}
+const addFormListeners = () => {}
+const addBackListener = () => {}
+
+
+
+
