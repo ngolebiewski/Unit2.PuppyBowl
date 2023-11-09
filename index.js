4343f2af162a804ef7de6502fc546475a4786462
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-09 11:54:14 -0500

    added fetch functions, a sweet puppy bowl header image, and a state object.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-console.log("Hello World");
+//make state
+const state = {
+  dogPlayers: [],
+  teams: [], //team ruff and team fluff LOL!
+  currentPlayer: [],
+}
+
+////////////////////////
+/// GLOBAL VARIABLES ///
+////////////////////////
+const baseURL = `https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web-ft-sf/`;
+const main = document.querySelector(`main`);
+
+////////////////////////
+/// FETCH FUNCTIONS  ///
+////////////////////////
+
+//fetch data from API, LOL, fetch...dogs play fetch. 
+const getPlayersFromAPI = async () => {
+  try {
+    const response = await fetch(`${baseURL}players`);
+    const responseJSON = await response.json();
+    state.dogPlayers = responseJSON.data.players;
+    //add error stuff here!!!!
+  } catch (error) {
+    console.log(`error`);
+  }
+};
+
+const getTeamsFromAPI = async () => {
+  try {
+    const response = await fetch(`${baseURL}teams`);
+    const responseJSON = await response.json();
+    state.teams = responseJSON.data.teams;
+    console.log(state.teams);
+    //add error stuff here!!!!
+  } catch (error) {
+    console.log(`error`);
+  }
+};
+
+//I'm getting the individual player from the API to show I can, although I think I'll mainly be using the players from state
+const getIndividualPlayerFromAPI = async (playerID) => {
+  try {
+    const response = await fetch(`${baseURL}players/${playerID}`);
+    const responseJSON = await response.json();
+    state.currentPlayer = responseJSON.data.player;
+    console.log(state.currentPlayer);
+    //add error stuff here!!!!
+  } catch (error) {
+    console.log(`error`);
+  }
+};
+
+getIndividualPlayerFromAPI(229);
